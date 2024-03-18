19e20cf8c8dc84894a27fb5e0ce55c60b9b59ebe
Pawan Kumar <pyadav.workmail@gmail.com>
2024-03-19 01:13:18 +0530

    new batch conmtent updated

--- src/Pages/newBatch/newBatchPrice/oneYearPrice/OneYearPrice.jsx
+++ src/Pages/newBatch/newBatchPrice/oneYearPrice/OneYearPrice.jsx
@@ -13,41 +13,38 @@ import PlatinumOneYearSection from '../../newBatchPrice/platinum/oneYear/Platinu
 
 const newsAndUpdates = [
     {
-        url: "/testseries/csir-net",
-        content: "Test Series for CSIR NET Dec 2023 is now live. Sharpen your skills and boost your preparation by joining now!            "
-    },
-    {
-        url: "/testseries/gate",
-        content: "Test Series for GATE 2024 is live. Don't miss this opportunity to practice and enhance your exam readiness. Join now!            "
-    },
-    {
-        url: "/testseries/iit-jam",
-        content: "Test Series for IIT JAM 2024 is now available. Enroll today to assess your preparation and improve your performance!"
+        url: "/onlinecourses/csir-net",
+        content: "CSIR NET Chemical Sciences Online Batches"
     },
     {
         url: "/offlinecourses/csir-net",
-        content: "CSIR NET June 2024 Offline Batch is set to commence on January 5, 2023. Register now for in-person classes and personalized guidance."
-    },
-    {
-        url: "/onlinecourses/csir-net",
-        content: "CSIR NET June 2024 Online Batch starts on January 5, 2023. Secure your place for online learning with our experienced faculty."
-    },
-    {
-        url: "/offlinecourses/gate",
-        content: "GATE 2025 Offline Batch begins on February 18, 2023. Register early to reserve your spot for comprehensive offline coaching."
+        content: "CSIR NET Chemical Sciences Offline Course"
     },
     {
         url: "/onlinecourses/gate",
-        content: "GATE 2025 Online Batch also starts on February 18, 2023. Join our online classes for flexible and effective exam preparation."
+        content: "GATE Chemistry Online Batch"
     },
     {
-        url: "/offlinecourses/iit-jam",
-        content: "IIT JAM 2025 Offline Batch is scheduled to begin on February 18, 2023. Registration is open for in-person coaching sessions."
+        url: "/offlinecourses/gate",
+        content: "GATE Chemistry Offline Batch"
     },
     {
         url: "/onlinecourses/iit-jam",
-        content: "IIT JAM 2025 Online Batch starts on February 18, 2023. Register now for our online classes and access quality education from the comfort of your home."
+        content: "IIT-JAM Chemistry Online Batch"
     },
+    {
+        url: "/offlinecourses/iit-jam",
+        content: "IIT-JAM Chemistry Offline Batch"
+    },
+    {
+        url: "/onlinecourses/cuet",
+        content: "CUET(PG) Chemistry Online Batch"
+    },
+    {
+        url: "/offlinecourses/cuet",
+        content: "CUET(PG) Chemistry Offline Batch "
+    },
+
 
 ]
 
